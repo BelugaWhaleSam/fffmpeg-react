205f3b294b993ce3ce576e8cdb5a4cc64202bf93
Mohd Sameer <sameermohd2k1@gmail.com>
2023-01-03 21:28:34 +0530

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,16 @@ function App() {
       "5", // fragment duration second
       "-ss",
       "10.0",
-      
+      "-filter_complex",
+      "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2.0[a]",
+      "-map",
+      "[v]",
+      "-map",
+      "[a]",
+      "-deadline",
+      "realtime",
+      "-cpu-used",
+      "8",
       "-f",
       "mp4",
 
