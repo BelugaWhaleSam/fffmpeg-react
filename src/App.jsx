2675f57dfef28b77258aad7ba15606183c638534
Mohd Sameer <sameermohd2k1@gmail.com>
2023-01-04 18:08:43 +0530

    dynamic speed

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
   const [encoded, setEncoded] = useState();
+  const [duration, setDuration] = useState(0);
 
   const load = async () => {
     await ffmpeg.load();
@@ -22,18 +23,37 @@ function App() {
     // Write the file to memory
     ffmpeg.FS("writeFile", "inputFile.mp4", await fetchFile(video));
 
+    // const vf = 0.5; // 2x
+    // const af = 2.0; // 2x
+
+    const videoFast = () => {
+      if (duration <= 300) {
+        return 0.2;  // 5x
+      } else {
+        return 0.1; // 10x
+      }
+    }
+
+    const audioFast = () => {
+      if (duration <= 300) {
+        return 5;  // 5x
+      } else {
+        return 10; // 10x
+      }
+    }
+
     // Run the FFMpeg command
     await ffmpeg.run(
       "-i",
       "inputFile.mp4",
       "-preset",
       "ultrafast",
-      "-t",
-      "5", // fragment duration second
+      // "-t",
+      // "5", // fragment duration second
       "-ss",
-      "10.0",
+      "0.0",
       "-filter_complex",
-      "[0:v]setpts=0.5*PTS[v];[0:a]atempo=2.0[a]",
+      `[0:v]setpts=${videoFast()}*PTS[v];[0:a]atempo=${audioFast()}[a]`,
       "-map",
       "[v]",
       "-map",
@@ -56,12 +76,17 @@ function App() {
       new Blob([data.buffer], { type: "video/mp4" })
     );
     setEncoded(url);
+    console.log("duration", duration);
   };
 
   return ready ? (
     <div className="App">
+
       {video && (
-        <video controls width="250" src={URL.createObjectURL(video)}></video>
+        <video controls width="250" src={URL.createObjectURL(video)}
+        onLoadedMetadata={e => {
+          setDuration(e.target.duration)
+        }}></video>
       )}
 
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
